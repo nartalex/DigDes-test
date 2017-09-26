b18ec6a0caf221f70bf3a7244d489e429e0943a8
Nartalex <nartalex@ya.ru>
2017-09-26 10:44:17 +0300

    Доделал интерфейс Сделал расчет пропорций нового изображения

--- DigDes/Form1.Designer.cs
+++ DigDes/Form1.Designer.cs
@@ -31,14 +31,22 @@
             this.openFileButton = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.sourcePreview = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageHorSizeUpDown = new System.Windows.Forms.NumericUpDown();
+            this.generateAsciiButton = new System.Windows.Forms.Button();
+            this.horizontalSizeRadio = new System.Windows.Forms.RadioButton();
+            this.verticalSizeRadio = new System.Windows.Forms.RadioButton();
+            this.imageVerSizeUpDown = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.sourcePreview)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageHorSizeUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageVerSizeUpDown)).BeginInit();
             this.SuspendLayout();
             // 
             // openFileButton
             // 
-            this.openFileButton.Location = new System.Drawing.Point(103, 127);
+            this.openFileButton.Location = new System.Drawing.Point(26, 21);
             this.openFileButton.Name = "openFileButton";
-            this.openFileButton.Size = new System.Drawing.Size(111, 23);
+            this.openFileButton.Size = new System.Drawing.Size(129, 27);
             this.openFileButton.TabIndex = 0;
             this.openFileButton.Text = "Открыть файл";
             this.openFileButton.UseVisualStyleBackColor = true;
@@ -50,24 +58,126 @@
             // 
             // sourcePreview
             // 
-            this.sourcePreview.Location = new System.Drawing.Point(343, 12);
+            this.sourcePreview.Location = new System.Drawing.Point(435, 21);
             this.sourcePreview.Name = "sourcePreview";
-            this.sourcePreview.Size = new System.Drawing.Size(250, 250);
+            this.sourcePreview.Size = new System.Drawing.Size(300, 300);
             this.sourcePreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.sourcePreview.TabIndex = 1;
             this.sourcePreview.TabStop = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Размер ASCII изображения:";
+            // 
+            // imageHorSizeUpDown
+            // 
+            this.imageHorSizeUpDown.Location = new System.Drawing.Point(156, 115);
+            this.imageHorSizeUpDown.Maximum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.imageHorSizeUpDown.Minimum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.imageHorSizeUpDown.Name = "imageHorSizeUpDown";
+            this.imageHorSizeUpDown.Size = new System.Drawing.Size(140, 23);
+            this.imageHorSizeUpDown.TabIndex = 3;
+            this.imageHorSizeUpDown.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            // 
+            // generateAsciiButton
+            // 
+            this.generateAsciiButton.Enabled = false;
+            this.generateAsciiButton.Location = new System.Drawing.Point(26, 274);
+            this.generateAsciiButton.Name = "generateAsciiButton";
+            this.generateAsciiButton.Size = new System.Drawing.Size(126, 47);
+            this.generateAsciiButton.TabIndex = 4;
+            this.generateAsciiButton.Text = "Сгенерировать";
+            this.generateAsciiButton.UseVisualStyleBackColor = true;
+            this.generateAsciiButton.Click += new System.EventHandler(this.generateAsciiButton_Click);
+            // 
+            // horizontalSizeRadio
+            // 
+            this.horizontalSizeRadio.AutoSize = true;
+            this.horizontalSizeRadio.Checked = true;
+            this.horizontalSizeRadio.Location = new System.Drawing.Point(27, 115);
+            this.horizontalSizeRadio.Name = "horizontalSizeRadio";
+            this.horizontalSizeRadio.Size = new System.Drawing.Size(114, 19);
+            this.horizontalSizeRadio.TabIndex = 5;
+            this.horizontalSizeRadio.TabStop = true;
+            this.horizontalSizeRadio.Text = "По горизонтали";
+            this.horizontalSizeRadio.UseVisualStyleBackColor = true;
+            this.horizontalSizeRadio.CheckedChanged += new System.EventHandler(this.horizontalSizeRadio_CheckedChanged);
+            // 
+            // verticalSizeRadio
+            // 
+            this.verticalSizeRadio.AutoSize = true;
+            this.verticalSizeRadio.Location = new System.Drawing.Point(27, 155);
+            this.verticalSizeRadio.Name = "verticalSizeRadio";
+            this.verticalSizeRadio.Size = new System.Drawing.Size(101, 19);
+            this.verticalSizeRadio.TabIndex = 7;
+            this.verticalSizeRadio.Text = "По вертикали";
+            this.verticalSizeRadio.UseVisualStyleBackColor = true;
+            this.verticalSizeRadio.CheckedChanged += new System.EventHandler(this.verticalSizeRadio_CheckedChanged);
+            // 
+            // imageVerSizeUpDown
+            // 
+            this.imageVerSizeUpDown.Enabled = false;
+            this.imageVerSizeUpDown.Location = new System.Drawing.Point(156, 155);
+            this.imageVerSizeUpDown.Maximum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.imageVerSizeUpDown.Minimum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.imageVerSizeUpDown.Name = "imageVerSizeUpDown";
+            this.imageVerSizeUpDown.Size = new System.Drawing.Size(140, 23);
+            this.imageVerSizeUpDown.TabIndex = 6;
+            this.imageVerSizeUpDown.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AcceptButton = this.generateAsciiButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(672, 413);
+            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.verticalSizeRadio);
+            this.Controls.Add(this.imageVerSizeUpDown);
+            this.Controls.Add(this.horizontalSizeRadio);
+            this.Controls.Add(this.generateAsciiButton);
+            this.Controls.Add(this.imageHorSizeUpDown);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.sourcePreview);
             this.Controls.Add(this.openFileButton);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "BitmapToASCII";
             ((System.ComponentModel.ISupportInitialize)(this.sourcePreview)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageHorSizeUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageVerSizeUpDown)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -76,6 +186,12 @@
         private System.Windows.Forms.Button openFileButton;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.PictureBox sourcePreview;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown imageHorSizeUpDown;
+        private System.Windows.Forms.Button generateAsciiButton;
+        private System.Windows.Forms.RadioButton horizontalSizeRadio;
+        private System.Windows.Forms.RadioButton verticalSizeRadio;
+        private System.Windows.Forms.NumericUpDown imageVerSizeUpDown;
     }
 }
 
--- DigDes/Form1.cs
+++ DigDes/Form1.cs
@@ -30,7 +30,32 @@ namespace DigDes
             {
                 source = new Bitmap(dialog.FileName);
                 sourcePreview.Image = source;
+                generateAsciiButton.Enabled = true;
             }
+        }
+
+        private void generateAsciiButton_Click(object sender, EventArgs e)
+        {
+            double ratio = (double)source.Width / source.Height;
+            int width = horizontalSizeRadio.Checked
+                ? (int)imageHorSizeUpDown.Value
+                : (int)Math.Round((int)imageVerSizeUpDown.Value / ratio);
+            int height = verticalSizeRadio.Checked
+                ? (int)imageVerSizeUpDown.Value
+                : (int)Math.Round((int)imageHorSizeUpDown.Value / ratio);
+            MessageBox.Show($"Old res: {source.Width} : {source.Height} \r\nNew res: {width} : {height}");
+
+
+        }
+
+        private void horizontalSizeRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            imageHorSizeUpDown.Enabled = horizontalSizeRadio.Checked;
+        }
+
+        private void verticalSizeRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            imageVerSizeUpDown.Enabled = verticalSizeRadio.Checked;
         }
     }
 }
